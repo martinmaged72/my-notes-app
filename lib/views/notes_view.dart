0b77c0c6387b84aca5e215e1574b9ea7552a556d
marti <Martinmaged72@gmail.com>
2024-08-28 14:45:31 +0300

    add+ FAB

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:noteted/widgets/NotesViewBody.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add,),
+      ),
+      body: const NotesViewBody(),
+    );
   }
 }
